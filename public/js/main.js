09d867ef6ad697c954f94262652639c870d27a1b
Saiyann <knt4xyz@gmail.com>
2021-08-19 22:03:01 +0200

    ✅Implemanted ioSocket functionto render messages in the messages container
    implemant scroll for readibility and focus to continue writting new messages in an empty field.
    
    use outputMessage() Methode to retrieve io.emit('msgSent to server') 
    
    created the messages.js  utils directory to identify users : username, text messages and time

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,49 @@
-//io() is accessible cause of teh script we added in chat.html
+const chatForm = document.getElementById('chat-form');
+const allMsgContainerDiv = document.querySelector('.chatMsgsContainer');
+
+
+// io() is accessible cause of teh script we added in chat.html
 const socket = io();
+
+// Message from server
+socket.on('message_sent', message => {
+    console.log(message);
+    outputMessage(message);
+
+    // ScrollDown to alway make the last message visib;e:
+    allMsgContainerDiv.scrollTop = allMsgContainerDiv.scrollHeight;
+});
+
+socket.on('welcome_msg', message => {
+    console.log(message);
+});
+
+socket.on('hasJoined_msg', message => {
+    console.log(message);
+});
+
+socket.on('disconnect_msg', message => {
+    console.log(message);
+});
+
+// Message submit to server
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const msgChatInputField = e.target.elements.msgId.value;
+
+    // console.log(msgChatInputField);
+    // Emit the message to the server
+    socket.emit('chat_input_msg', msgChatInputField);
+
+    e.target.elements.msgId.value = '';
+    e.target.elements.msgId.focus();
+});
+
+// Output message to Dom
+function outputMessage(message) {
+        const div = document.createElement('div');
+        div.classList.add('msgSent');
+        div.innerHTML = `<p class="text">${message}</p>`
+        document.querySelector('.chatMsgsContainer').appendChild(div);
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require ('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const formatmessage = require('./utils/messages')
 
 const app = express();
 // we the http module to allow io.socket to work properly
@@ -9,11 +10,29 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Setting acces to the public static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when a client connect
 io.on('connection', socket => {
-    console.log('New connection established');
+    // console.log('Test New connection established');
+
+    // send the message to the user connecting
+    socket.emit('welcome_msg', 'Welcome to SaYannChat App');
+
+    // broadcast when a user connects (brodcast to everybody except the sender)
+    socket.broadcast.emit('hasJoined_msg', 'A user has joined the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        // io.emit(): brodcast to everybody
+        io.emit('disconnect_msg', 'A user has left the chat');
+    });
+
+    // Listen for Chat input message
+    socket.on('chat_input_msg', message => {
+        console.log(message);
+        io.emit('message_sent', message);
+    });
 });
 
 const PORT = 3100 || process.env.port;
